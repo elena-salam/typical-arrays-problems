684d9cd93d12345db6e570e507a74af47ddbaa3c
elena-salam <elena.salamakha.ua@gmail.com>
2021-03-11 10:37:17 +0200

    feat: add solutions for functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array===undefined || array===null ||array.length<=0){
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
+
+
 exports.max = function max (array) {
-  return 0;
+  if(array===undefined || array===null ||array.length<=0){
+    return 0;
+  } else {
+    const a = array.sort((a, b)=>b-a)
+  return(a[0])
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array===undefined || array===null ||array.length<=0){
+    return 0;
+  } else {
+    const a = array.reduce((acc, item) => {
+      return acc+item}
+      , 0);
+      return a/array.length
+    
+  }
 }
